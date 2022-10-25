38a01b9b4ffacb822d7ea1995a4cfcb7bde70d06
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-25 20:20:29 +0300

    Добавьте файлы проекта.

---
+++ ЛР1ИДЗ/LR1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    internal class LR1
+    {
+        public static void DoTask()
+        {
+            Console.Write("Введите количество чисел ");
+            int n = int.Parse(Console.ReadLine());
+            float [] numb = new float[n];
+            float [] numb2 = new float[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите "+(i+1)+" число ");
+                numb[i] = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Результирующая последовательность: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    numb2[i] += numb[j];
+                }
+                Console.WriteLine(numb2[i]);
+            }
+        }
+    }
+}
---
+++ ЛР1ИДЗ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            LR1.DoTask();
+        }
+    }
+}
